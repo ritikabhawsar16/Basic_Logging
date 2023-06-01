631805e38b8aa0727a7e3c080a87145dbd5a01d0
ritikabhawsar16 <ritikabhawsar.adt@gmail.com>
2023-06-01 10:24:58 +0530

    Basic Logger Impl Using SLF4J

---
+++ src/main/java/com/learnlogging/LoggerImplementationApplication.java
@@ -0,0 +1,14 @@
+package com.learnlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LoggerImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggerImplementationApplication.class, args);
+	}
+
+}
